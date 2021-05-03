0b0cbd826de7b2de8866e826ff55ff7cfa3fea36
RameetB <71399669+RameetB@users.noreply.github.com>
2021-05-03 00:41:27 -0600

    settings  menu functionality complete, note from JD needs apply button

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -15,14 +15,23 @@ public class OptionsMenu : MonoBehaviour{
         resolutionsDropdown.ClearOptions();
 
         List<string> options = new List<string> ();
-
+        int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++) {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height) {
+                currentResolutionIndex = i;
+            }
         }
         resolutionsDropdown.AddOptions(options);
+        resolutionsDropdown.value = currentResolutionIndex;
+        resolutionsDropdown.RefreshShownValue();
     }
 
+    public void SetResolution (int resolutionIndex) {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
     //set the volume slider to change the audio mixer, -80, 100 min max
     public AudioMixer audioMixer;
     public void SetVolume (float volume) {
